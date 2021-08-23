546d3efc4ac2c85202b4fbb61c83ce6fd2f3148f
Sm Sadi <smsadi19@gmail.com>
2021-08-24 00:09:16 +0200

    if and else if

--- simple.sh
+++ simple.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+# print 
 echo "hello world"
 
-Name="sadi"
+#Variables
+NAME="sadi"
+echo "my name is $NAME"
+SPORT_01="football"
+echo "my favourite game is ${SPORT_01}"
+
+# take user input 
+read -p "what is your number " NUMBER
+echo "your number is ${NUMBER}"
+
+# taking multiple user input 
+echo "what is your full name: "
+read FNAME LNAME
+echo "your name is $FNAME $LNAME"
+
+# if statement 
+if [ "$NAME" = "sadi"];
+then
+echo "welcome sadi"
+fi
+
+# if else 
+if [ "$NAME" = "sadi"];
+then
+echo "welcome"
+else
+echo "welcome whoever you are "
+fi
+
